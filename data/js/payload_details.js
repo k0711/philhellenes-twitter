ea8fef4241158683e1b77d0c1ba7be57c1ac7a0b
Keith Frankish <k.frankish@gmail.com>
2018-11-13 03:08:39 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 394,
-	"created_at": "Mon Nov 12 01:08:38 +0000 2018",
+	"tweets": 395,
+	"created_at": "Tue Nov 13 01:08:38 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -70,6 +70,123 @@ Grailbird.data.tweets_2018_11 =
 				}
 			},
 			"protected": false,
+			"followers_count": 15165,
+			"friends_count": 1063,
+			"listed_count": 353,
+			"created_at": "Wed Apr 29 17:12:31 +0000 2009",
+			"favourites_count": 2071,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4677,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E0DFB3",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1719345789/Dscn6047-rot-facecrop_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1719345789/Dscn6047-rot-facecrop_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/36411199/1375623658",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 23,
+		"favorite_count": 46,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "el",
+		"text": "Με την ευκαιρία των σημερινών εγκαινίων της νέας στέγης για το Ελληνικό Μουσείο Πληροφορικής, διαθέτουμε χωρίς περιορισμό πρόσβασης (Green OA) το τελικό δοκίμιο του δημοσιευμένου άρθρου μας Aspects of the History of computing in modern Greece στο https://t.co/IJwZGaKDPb @mouseio"
+	},
+	{
+		"created_at": "Wed Nov 07 17:00:00 +0000 2018",
+		"id": 1060215302225625100,
+		"id_str": "1060215302225625089",
+		"full_text": "Με την ευκαιρία των σημερινών εγκαινίων της νέας στέγης για το Ελληνικό Μουσείο Πληροφορικής, διαθέτουμε χωρίς περιορισμό πρόσβασης (Green OA) το τελικό δοκίμιο του δημοσιευμένου άρθρου μας Aspects of the History of computing in modern Greece στο https://t.co/IJwZGaKDPb @mouseio",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mouseio",
+					"name": "Ελληνικό Μουσείο Πληροφορικής",
+					"id": 110506013,
+					"id_str": "110506013",
+					"indices": [
+						271,
+						279
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/IJwZGaKDPb",
+					"expanded_url": "http://www.dmst.aueb.gr/dds/pubs/jrnl/2018-AnnalsofComputerHistory-GreekInformatics/html/DMS18.pdf",
+					"display_url": "dmst.aueb.gr/dds/pubs/jrnl/…",
+					"indices": [
+						247,
+						270
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 36411199,
+			"id_str": "36411199",
+			"name": "Διομήδης Σπινέλλης",
+			"screen_name": "DSpinellis",
+			"location": "Athens, Greece",
+			"description": "Καθηγητής και πρόεδρος του Τμήματος ΔΕΤ στο Οικονομικό Πανεπιστήμιο Αθηνών. Account for Greek/Greece tweets. For English/IT tweets in English follow @CoolSWEng.",
+			"url": "http://t.co/zCoAayxPml",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/zCoAayxPml",
+							"expanded_url": "http://www.spinellis.gr",
+							"display_url": "spinellis.gr",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 15167,
 			"friends_count": 1063,
 			"listed_count": 353,
